d532174e93227a82e50b40fb7d29028fd6fbb6b7
JILZXY <sasosiso79@gmail.com>
2025-11-11 13:53:32 -0600

    arreglar archivos

--- app/src/main/java/com/example/notespote/ui/theme/Theme.kt
+++ app/src/main/java/com/example/notespote/ui/theme/Theme.kt
@@ -3,7 +3,8 @@ package com.example.notespote.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
