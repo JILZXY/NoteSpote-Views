0ee29060bc38b308ed86f7eb489bdc47301f06d2
JILZXY <Sasosiso79@gmail.com>
2025-11-11 14:21:17 -0600

    NavManager creado

--- app/src/main/java/com/example/notespote/MainActivity.kt
+++ app/src/main/java/com/example/notespote/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NoteSpoteTheme {
-                println("hols")
+
             }
         }
     }
