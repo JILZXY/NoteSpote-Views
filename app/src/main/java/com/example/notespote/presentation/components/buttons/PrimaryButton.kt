0ed6ad8c60cfbd45094498f4a0ed3011ebe12881
luvia <luviaamagaali07@gmail.com>
2025-11-19 23:58:05 -0600

    Boton Primario creado

--- app/src/main/java/com/example/notespote/presentation/components/buttons/PrimaryButton.kt
+++ app/src/main/java/com/example/notespote/presentation/components/buttons/PrimaryButton.kt
@@ -1,2 +1,42 @@
-package com.example.notespote.presentation.components.buttons
+package com.example.notespot.presentation.components.buttons
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.notespot.presentation.theme.*
+import com.example.notespote.presentation.theme.Celeste
+import com.example.notespote.presentation.theme.RichBlack
+
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Celeste,
+            contentColor = RichBlack
+        )
+    ) {
+        Text(
+            text = text,
+            fontFamily = FontFamily.Monospace,
+            fontSize = 16.sp
+        )
+    }
+}
