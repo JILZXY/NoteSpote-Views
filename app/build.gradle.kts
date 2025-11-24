ca62692f10383f4c71f09589162d1afea508e102
JILZXY <Sasosiso79@gmail.com>
2025-11-24 11:52:29 -0600

    dependencias de firebase implementadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -103,5 +104,7 @@ dependencies {
 
     implementation("androidx.compose.ui:ui-text-google-fonts:1.7.5")
 
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
 }
