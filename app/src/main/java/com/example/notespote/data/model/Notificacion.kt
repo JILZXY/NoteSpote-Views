b456218c9ab5d1ac9f781bdbcede3827ab93cae7
JILZXY <Sasosiso79@gmail.com>
2025-11-27 00:53:35 -0600

    Se agrego el Model de Notificacion

---
+++ app/src/main/java/com/example/notespote/data/model/Notificacion.kt
@@ -0,0 +1,18 @@
+package com.example.notespote.data.model
+
+data class Notificacion(
+    val id: String = "",
+    val idUsuario: String = "",
+    val tipo: TipoNotificacion,
+    val titulo: String = "",
+    val mensaje: String = "",
+    val idReferencia: String? = null, // ID del apunte, usuario, etc
+    val leida: Boolean = false,
+    val fecha: Long = System.currentTimeMillis()
+)
+
+enum class TipoNotificacion {
+    NUEVO_SEGUIDOR,
+    LIKE_APUNTE,
+    APUNTE_GUARDADO,
+}
