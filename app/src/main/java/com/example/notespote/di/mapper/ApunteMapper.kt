4bc3b13d982423c19ebfcc374e77c9295592c8f6
JILZXY <Sasosiso79@gmail.com>
2025-11-26 19:11:39 -0600

    PostitMapper y postitDAo agregado

--- app/src/main/java/com/example/notespote/di/mapper/ApunteMapper.kt
+++ app/src/main/java/com/example/notespote/di/mapper/ApunteMapper.kt
@@ -1,6 +1,8 @@
 package com.example.notespote.di.mapper
 
 import com.example.notespote.data.local.entities.ApunteEntity
+import com.example.notespote.data.local.entities.SyncStatus
+import com.example.notespote.data.local.entities.TipoVisibilidad
 import com.example.notespote.data.remote.dto.ApunteDto
 import com.google.firebase.Timestamp
 import java.util.Date
@@ -25,6 +27,31 @@ fun ApunteEntity.toDto(): ApunteDto {
         tieneImagenes = tieneImagenes,
         tienePostits = tienePostits,
         updatedAt = Timestamp.now(),
+        isDeleted = isDeleted
+    )
+}
+
+fun ApunteDto.toEntity(): ApunteEntity {
+    return ApunteEntity(
+        idApunte = id,
+        idUsuario = idUsuario,
+        idMateria = idMateria,
+        idCarpeta = idCarpeta,
+        titulo = titulo,
+        contenido = contenido,
+        tipoVisibilidad = TipoVisibilidad.valueOf(tipoVisibilidad),
+        esOriginal = esOriginal,
+        idApunteOriginal = idApunteOriginal,
+        idUsuarioOriginal = idUsuarioOriginal,
+        totalLikes = totalLikes,
+        totalGuardados = totalGuardados,
+        fechaCreacion = fechaCreacion.toDate().time,
+        fechaActualizacion = fechaActualizacion?.toDate()?.time,
+        tieneDibujos = tieneDibujos,
+        tieneImagenes = tieneImagenes,
+        tienePostits = tienePostits,
+        syncStatus = SyncStatus.SYNCED,
+        lastSync = System.currentTimeMillis(),
         isDeleted = isDeleted
     )
 }
---
+++ app/src/main/java/com/example/notespote/di/mapper/PostitMapper.kt
@@ -0,0 +1,21 @@
+package com.example.notespote.di.mapper
+
+import com.example.notespote.data.local.entities.PostitEntity
+import com.google.firebase.Timestamp
+import java.util.Date
+
+fun PostitEntity.toDto(): PostitDto {
+    return PostitDto(
+        id = idPostit,
+        tituloPostit = tituloPostit,
+        contenidoPostit = contenidoPostit,
+        color = color,
+        posicionX = posicionX,
+        posicionY = posicionY,
+        ancho = ancho,
+        alto = alto,
+        fechaCreacion = Timestamp(Date(fechaCreacion)),
+        ordenZ = ordenZ,
+        isDeleted = isDeleted
+    )
+}
