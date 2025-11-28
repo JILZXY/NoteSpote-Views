3569ee4f6de25af7d397ba43c764aa46b9a4f326
JILZXY <Sasosiso79@gmail.com>
2025-11-28 11:55:31 -0600

    Usecase de likes, GetLikesByApunte

--- app/src/main/java/com/example/notespote/domain/usecases/likes/GetLikesByApunteUseCase.kt
+++ app/src/main/java/com/example/notespote/domain/usecases/likes/GetLikesByApunteUseCase.kt
@@ -1,2 +1,15 @@
+// domain/usecases/likes/GetLikesByApunteUseCase.kt
 package com.example.notespote.domain.usecases.likes
 
+import com.example.notespote.domain.model.Like
+import com.example.notespote.domain.repository.LikeRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetLikesByApunteUseCase @Inject constructor(
+    private val likeRepository: LikeRepository
+) {
+    operator fun invoke(apunteId: String): Flow<Result<List<Like>>> {
+        return likeRepository.getLikesByApunte(apunteId)
+    }
+}
