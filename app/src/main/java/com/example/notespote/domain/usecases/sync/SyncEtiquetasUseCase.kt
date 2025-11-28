6cbb5b30ef027dc7d92b3066371224dabf8ebbe6
JILZXY <Sasosiso79@gmail.com>
2025-11-28 11:52:00 -0600

    Usecase de sync, SyncEtiqueta

---
+++ app/src/main/java/com/example/notespote/domain/usecases/sync/SyncEtiquetasUseCase.kt
@@ -0,0 +1,13 @@
+// domain/usecases/sync/SyncEtiquetasUseCase.kt
+package com.example.notespote.domain.usecases.sync
+
+import com.example.notespote.domain.repository.SyncRepository
+import javax.inject.Inject
+
+class SyncEtiquetasUseCase @Inject constructor(
+    private val syncRepository: SyncRepository
+) {
+    suspend operator fun invoke(): Result<Unit> {
+        return syncRepository.syncEtiquetas()
+    }
+}
