b1b5c0a024f095d02f0dc45fba9b45d863af3fd0
luvia <luviaamagaali07@gmail.com>
2025-12-08 11:34:41 -0600

    Integracion del firebase (Logs)

--- app/src/main/java/com/example/notespote/NoteSpoteApplication.kt
+++ app/src/main/java/com/example/notespote/NoteSpoteApplication.kt
@@ -1,7 +1,17 @@
 package com.example.notespote
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NoteSpoteApplication : Application()
+class NoteSpoteApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Inicializar Firebase al iniciar la app
+        FirebaseApp.initializeApp(this)
+
+        android.util.Log.d("NoteSpotApp", "App iniciada - Firebase configurado")
+    }
+}
